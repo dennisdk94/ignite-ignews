3f67e5672d7a211ace1e879d299b898ae506af8b
Dennis Dyodi Kawakami <dennis.kawakami@externos-br.cencosud.com>
2022-04-21 16:18:12 -0300

    fundamentos next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
@@ -36,7 +36,10 @@ export default function Home( { product }: HomeProps) {
   )
 }
 
-export const getServerSideProps:GetServerSideProps = async () => {
+//getServerSideProps -> SSR (pagina dinamica)
+//getStaticProps -> SSG (pagina estatica)
+
+export const getStaticProps:GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1Kr4W8HiOfX1EJITZYU1nBHS', {
     expand: ['product']
   });
@@ -52,6 +55,7 @@ export const getServerSideProps:GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24 //24 hours
   }
 }
