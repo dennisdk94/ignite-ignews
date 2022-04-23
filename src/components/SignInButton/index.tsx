ebd2ce7daf149665aefa5154b5b3a4bcbcfe74c3
Dennis Dyodi Kawakami <dennis.kawakami@externos-br.cencosud.com>
2022-04-23 12:51:35 -0300

    iniciando backend

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,29 @@
-import styles from './styles.module.scss';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, useSession, signOut } from 'next-auth/react';
+
+import styles from './styles.module.scss';
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    const { data } = useSession();
 
-    return isUserLoggedIn ? (
-        <button className={styles.signInButton} type="button">
+    console.log(data);
+    return data ? (
+        <button 
+            className={styles.signInButton} 
+            type="button" 
+            onClick={() => signOut()}  
+        >
             <FaGithub color="#04D361" />
-            Dennis Kawakami
+                {data.user.name}
             <FiX color="##737380" className={styles.closeIcon} />
         </button>
     ) : (
-        <button className={styles.signInButton} type="button">
+        <button 
+            className={styles.signInButton} 
+            type="button"
+            onClick={() => signIn('github')} 
+        >
             <FaGithub color="#EBA417" />
             Sign in with Github
         </button>
